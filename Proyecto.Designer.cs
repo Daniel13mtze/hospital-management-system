5c750fcab37ea40dbaebb6621a6476d02c74e8a6
Daniel Martinez Espinoza <daniel13.mtze@gmail.com>
2026-07-04 21:14:08 -0700

    Add files via upload

---
+++ Proyecto.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
+            this.gestionDeSalasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip4 = new System.Windows.Forms.MenuStrip();
+            this.sistemaContableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip5 = new System.Windows.Forms.MenuStrip();
+            this.gestiónDeSalasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.citasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.menuStrip3.SuspendLayout();
+            this.menuStrip4.SuspendLayout();
+            this.menuStrip5.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 184);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip2.Size = new System.Drawing.Size(1067, 46);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(306, 42);
+            this.sToolStripMenuItem.Text = "Registro de actividad";
+            this.sToolStripMenuItem.Click += new System.EventHandler(this.sToolStripMenuItem_Click);
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestionDeSalasToolStripMenuItem});
+            this.menuStrip3.Location = new System.Drawing.Point(0, 138);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip3.Size = new System.Drawing.Size(1067, 46);
+            this.menuStrip3.TabIndex = 2;
+            this.menuStrip3.Text = "menuStrip3";
+            // 
+            // gestionDeSalasToolStripMenuItem
+            // 
+            this.gestionDeSalasToolStripMenuItem.Name = "gestionDeSalasToolStripMenuItem";
+            this.gestionDeSalasToolStripMenuItem.Size = new System.Drawing.Size(310, 42);
+            this.gestionDeSalasToolStripMenuItem.Text = "Registro de pacientes";
+            this.gestionDeSalasToolStripMenuItem.Click += new System.EventHandler(this.gestionDeSalasToolStripMenuItem_Click);
+            // 
+            // menuStrip4
+            // 
+            this.menuStrip4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip4.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip4.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaContableToolStripMenuItem});
+            this.menuStrip4.Location = new System.Drawing.Point(0, 92);
+            this.menuStrip4.Name = "menuStrip4";
+            this.menuStrip4.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip4.Size = new System.Drawing.Size(1067, 46);
+            this.menuStrip4.TabIndex = 3;
+            this.menuStrip4.Text = "menuStrip4";
+            // 
+            // sistemaContableToolStripMenuItem
+            // 
+            this.sistemaContableToolStripMenuItem.Name = "sistemaContableToolStripMenuItem";
+            this.sistemaContableToolStripMenuItem.Size = new System.Drawing.Size(255, 42);
+            this.sistemaContableToolStripMenuItem.Text = "Sistema contable";
+            this.sistemaContableToolStripMenuItem.Click += new System.EventHandler(this.sistemaContableToolStripMenuItem_Click);
+            // 
+            // menuStrip5
+            // 
+            this.menuStrip5.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip5.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip5.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gestiónDeSalasToolStripMenuItem});
+            this.menuStrip5.Location = new System.Drawing.Point(0, 46);
+            this.menuStrip5.Name = "menuStrip5";
+            this.menuStrip5.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip5.Size = new System.Drawing.Size(1067, 46);
+            this.menuStrip5.TabIndex = 4;
+            this.menuStrip5.Text = "menuStrip5";
+            // 
+            // gestiónDeSalasToolStripMenuItem
+            // 
+            this.gestiónDeSalasToolStripMenuItem.Name = "gestiónDeSalasToolStripMenuItem";
+            this.gestiónDeSalasToolStripMenuItem.Size = new System.Drawing.Size(241, 42);
+            this.gestiónDeSalasToolStripMenuItem.Text = "Gestión de salas";
+            this.gestiónDeSalasToolStripMenuItem.Click += new System.EventHandler(this.gestiónDeSalasToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.citasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 46);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // citasToolStripMenuItem
+            // 
+            this.citasToolStripMenuItem.Name = "citasToolStripMenuItem";
+            this.citasToolStripMenuItem.Size = new System.Drawing.Size(94, 42);
+            this.citasToolStripMenuItem.Text = "Citas";
+            this.citasToolStripMenuItem.Click += new System.EventHandler(this.citasToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip3);
+            this.Controls.Add(this.menuStrip4);
+            this.Controls.Add(this.menuStrip5);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.menuStrip3.ResumeLayout(false);
+            this.menuStrip3.PerformLayout();
+            this.menuStrip4.ResumeLayout(false);
+            this.menuStrip4.PerformLayout();
+            this.menuStrip5.ResumeLayout(false);
+            this.menuStrip5.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem gestionDeSalasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip4;
+        private System.Windows.Forms.ToolStripMenuItem sistemaContableToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip5;
+        private System.Windows.Forms.ToolStripMenuItem gestiónDeSalasToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem citasToolStripMenuItem;
+    }
+}
+
---
+++ Proyecto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gestionDeSalasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NP np = new NP();
+            np.Show();
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void citasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Citas citas = new Citas();
+            citas.Show();
+        }
+
+        private void gestiónDeSalasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GS gs = new GS();
+            gs.Show();
+        }
+
+        private void sistemaContableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SC sc = new SC();
+            sc.Show();
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RA ra = new RA();
+            ra.Show();
+        }
+    }
+}
